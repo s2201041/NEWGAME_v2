8421b704d6ef3e9308c0f533e455814e545e72ae
s2201041 <ikt@ikt.jp>
2022-10-14 08:29:30 +0900

    更新

---
+++ NEWGAME/Base.cpp
@@ -0,0 +1,32 @@
+﻿#include "Base.h"
+
+Base::Base(Vec2 pos, Vec2 dir, int vel,int typ,Rect are)
+{
+	Pos = pos;	
+	Typ = typ;
+	Are = are;
+	Del = false;
+
+	//タイマーの初期化
+	Timer = 0;
+
+	//コライダーの初期化
+	Col = Circle{ Pos, size };
+
+}
+
+void Base::update()
+{
+	
+}
+
+void Base::draw() const
+{
+}
+
+void Base::sh_cla() {
+}
+
+void Base::pl_cla() {
+}
+
---
+++ NEWGAME/Base.h
@@ -0,0 +1,71 @@
+﻿#pragma once
+
+//インクルードガード
+#ifndef ENTITY_H
+#define ENTITY_H
+
+#include "common.h"
+
+class Base {
+
+public:
+
+	//コンストラクタ
+	Base(Vec2 pos, Vec2 dir, int vel,int typ,Rect are);
+
+	//１フレーム毎の処理
+	void update();
+
+	//描画
+	void draw() const ;
+
+	//諸突処理関数
+	void sh_cla();
+	void pl_cla();
+
+	//コライダー
+	Circle Col;
+
+	//仮死状態
+	bool Det;
+
+	//消去判定
+	bool Del;
+
+	//座標の定義
+	Vec2 Pos;
+
+	//直近座標
+	Vec2 PlPos;
+
+	//種類の定義
+	int Typ;
+
+	//HPの定義
+	int Hp;
+
+	//敵の名前の定義
+	String Nam;
+
+private:
+
+	Texture m_texture;
+
+	//座標と向きの定義
+	Vec2 Dir, GoPos;
+
+	//速度の定義
+	int Vel;
+
+	//コライダーサイズの定義
+	double size;
+
+	//タイマー変数の定義
+	double Timer;
+
+	//動作範囲
+	Rect Are;
+
+};
+
+#endif
